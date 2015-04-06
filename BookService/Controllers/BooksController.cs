549dc7ac07b0846a537367889d1b9b641b6fc49f
jmallinga <jmallinga@gmail.com>
2015-04-06 15:12:23 -0400

    Replaced GET methods in BookController with DTO's
    Next, replace the two GET methods in the BooksController class, with
    versions that return DTOs. We'll use the LINQ Select statement to
    convert from Book entities into DTOs.

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -20,14 +20,31 @@ namespace BookService.Controllers
         // GET: api/Books
         public IQueryable<Book> GetBooks()
         {
+    var books = from b in db.Books
+                select new BookDTO()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    AuthorName = b.Author.Name
+                };
+
             return db.Books;
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
+        [ResponseType(typeof(BookDetailDTO))]
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books.Include(b => b.Author).Select(b =>
+                new BookDetailDTO()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Year = b.Year,
+                    Price = b.Price,
+                    AuthorName = b.Author.Name,
+                    Genre = b.Genre
+                }).SingleOrDefaultAsync(b => b.Id == id);
             if (book == null)
             {
                 return NotFound();
